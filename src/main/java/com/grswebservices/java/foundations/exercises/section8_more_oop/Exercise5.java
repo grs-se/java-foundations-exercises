d213ea3be02c5df3b5bc1996a927c4854edf4a59
grs-se <george_ricesmith@hotmail.com>
2024-05-26 14:05:10 +0100

    Exercise 5

---
+++ src/main/java/com/grswebservices/java/foundations/exercises/section8_more_oop/Exercise5.java
@@ -0,0 +1,7 @@
+package com.grswebservices.java.foundations.exercises.section8_more_oop;
+
+public class Exercise5 {
+    public static char convert(int num) {
+        return (char)(num + 96);
+    }
+}
---
+++ src/test/java/com/grswebservices/java/foundations/exercises/section8_more_oop/Exercise5Test.java
@@ -0,0 +1,43 @@
+package com.grswebservices.java.foundations.exercises.section8_more_oop;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Exercise5Test {
+
+    @Test
+    public void convert1() {
+        char letter = Exercise5.convert(1);
+        assertEquals('a', letter);
+    }
+
+    @Test
+    public void convert26() {
+        char letter = Exercise5.convert(26);
+        assertEquals('z', letter);
+    }
+
+    @Test
+    public void convert23() {
+        char letter = Exercise5.convert(23);
+        assertEquals('w', letter);
+    }
+
+    @Test
+    public void convert6() {
+        char letter = Exercise5.convert(6);
+        assertEquals('f', letter);
+    }
+
+    @Test
+    public void convert3() {
+        char letter = Exercise5.convert(3);
+        assertEquals('c', letter);
+    }
+
+    @Test
+    public void convert8() {
+        char letter = Exercise5.convert(8);
+        assertEquals('h', letter);
+    }
+}
